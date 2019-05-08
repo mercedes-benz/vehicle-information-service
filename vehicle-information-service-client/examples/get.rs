337f59f7af29d2419a7a91cf6adf79ea48c53277
marcelbuesing <marcel.buesing@daimler.com>
2019-05-08 13:53:01 +0200

    Replace tokio- with runtime-crate in get client example.
    On behalf of Daimler TSS GmbH.

--- vehicle-information-service-client/examples/get.rs
+++ vehicle-information-service-client/examples/get.rs
@@ -1,16 +1,11 @@
+// SPDX-License-Identifier: MIT
+
 #![feature(await_macro, async_await)]
 
-use futures::future::{FutureExt, TryFutureExt};
-use tokio::runtime::Runtime;
 use vehicle_information_service_client::*;
 
-fn main() {
-    let mut rt = Runtime::new().unwrap();
-    let f = print_current_interval().unit_error().boxed().compat();
-    rt.block_on(f).unwrap().expect("Failed to receive get");
-}
-
-async fn print_current_interval() -> Result<(), VISClientError> {
+#[runtime::main]
+async fn main() -> Result<(), VISClientError> {
     let client = await!(VISClient::connect("ws://127.0.0.1:14430"))?;
     let interval: u32 = await!(client.get("Private.Example.Interval".into()))?;
     println!("Interval: {}", interval);
